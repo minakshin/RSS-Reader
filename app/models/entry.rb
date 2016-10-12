69ed736ea3ca58e7114bcf3f43c3ca030cadeb4e
Minakshi <minanajardhane@gmail.com>
2016-10-12 14:56:02 +0530

    Code corrections add spec for models, correct test cases for controller

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 # Entry model
 class Entry < ActiveRecord::Base
-  belongs_to :feed
+  belongs_to :feed, inverse_of: :entries
 
   scope :sort_by_published_date, lambda {
     order(published_date: :desc)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  ENV['AUTH_PASSWORD'] = 'rss_reader'
+  ENV['AUTH_USERNAME'] = 'rss_password'
 end
